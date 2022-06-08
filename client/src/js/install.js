1dc48e41b529e4df1122e0a844c3f13ff9878ac9
Jonny Gemme <gemmejonathan@gmail.com>
2022-06-07 21:53:50 -0400

    finished working on sw and wbpk

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,7 +2,16 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    if (isTooSoon) {
+        e.preventDefault(); // Prevents prompt display
+        // Prompt later instead:
+        setTimeout(function() {
+          isTooSoon = false;
+          e.prompt(); // Throws if called more than once or default not prevented
+        }, 10000);
+      }
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {});
